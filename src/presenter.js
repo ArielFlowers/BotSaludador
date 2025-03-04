1d8727d4603644141d6d550d9db849ddb41ba259
ArielFlowers <arielmakotoflores@gmail.com>
2025-03-04 19:24:35 -0400

    saluda con nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,7 +1,8 @@
 import saludar from "./saludador.js";
 
 document.querySelector("#saludar-btn").addEventListener("click", () => {
+    const nombre = document.querySelector("#nombre").value;
 
-    const saludo = saludar();
+    const saludo = saludar(nombre);
     document.querySelector("#resultado").innerText = saludo;
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,6 +1,9 @@
-function saludar() {
+function saludar(nombre) {
     let saludo = "Hola";
 
+    if (nombre) {
+        saludo += `, ${nombre}`;
+    }
     return saludo + "!";
 }
 export default saludar;
