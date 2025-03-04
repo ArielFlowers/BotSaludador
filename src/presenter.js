ea40b64f24ff99052b9d6724980d6df4a9c18b20
ArielFlowers <arielmakotoflores@gmail.com>
2025-03-04 19:22:46 -0400

    saluda con HOLA!

--- src/presenter.js
+++ src/presenter.js
@@ -1,25 +1,7 @@
-import sumar from "./sumador.js";
-import multiplicar from "./multiplicador.js";
+import saludar from "./saludador.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
-const selectOperation = document.querySelector("#operacion");
+document.querySelector("#saludar-btn").addEventListener("click", () => {
 
-const operaciones = {
-  sumar,
-  multiplicar,
-};
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-  const operacionSeleccionada = selectOperation.value;
-
-  const resultado = operaciones[operacionSeleccionada](firstNumber, secondNumber);
-
-  div.innerHTML = `<p>Resultado: ${resultado}</p>`;
+    const saludo = saludar();
+    document.querySelector("#resultado").innerText = saludo;
 });
---
+++ src/saludador.js
@@ -0,0 +1,6 @@
+function saludar() {
+    let saludo = "Hola";
+
+    return saludo + "!";
+}
+export default saludar;
