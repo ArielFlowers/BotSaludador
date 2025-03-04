0995698c894d17bbf3893440c60b823eaa886155
ArielFlowers <arielmakotoflores@gmail.com>
2025-03-04 19:27:26 -0400

    ahora saluda dependiendo la hora

--- src/saludador.js
+++ src/saludador.js
@@ -1,9 +1,15 @@
 function saludar(nombre) {
     let saludo = "Hola";
 
+    const hora = new Date().getHours();
+    if (hora < 12) saludo = "Buenos días";
+    else if (hora < 18) saludo = "Buenas tardes";
+    else saludo = "Buenas noches";
+    
     if (nombre) {
         saludo += `, ${nombre}`;
     }
+
     return saludo + "!";
 }
 export default saludar;
